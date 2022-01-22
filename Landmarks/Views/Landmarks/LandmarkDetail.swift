5c833f4997107daf9f5462f81031bb4c237e207a
DevBetterNet <40731171+DevBetterNet@users.noreply.github.com>
2022-01-22 23:19:38 +0700

    complete chapter (#4)
    Co-authored-by: KyleDev <macbook@chitmeo.com~>

--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
     var body: some View {
         VStack {
             MapView(coordinate: landmark.locationCoordinate)
@@ -18,8 +23,11 @@ struct LandmarkDetail: View {
                 .offset(y:-130)
                 .padding(.bottom, -130)
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 HStack {
                     Text(landmark.park)
                         .font(.subheadline)
@@ -45,6 +53,6 @@ struct LandmarkDetail: View {
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View{
-        LandmarkDetail(landmark: landmarks[1])
+        LandmarkDetail(landmark: ModelData().landmarks[1])
     }
 }
