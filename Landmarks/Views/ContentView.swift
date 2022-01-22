91eb95ed3243493ba6dd49d643e1f79f1caf83bb
KyleDev <macbook@chitmeo.com~>
2022-01-22 22:53:00 +0700

    add missing file

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Dev Better on 22/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
