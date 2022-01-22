7e181011056986f9244b61d069f484193fd692a0
KyleDev <macbook@chitmeo.com~>
2022-01-22 21:26:28 +0700

    SwiftUI Essentials Creating and Combining Views

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Dev Better on 22/01/2022.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(.white, lineWidth: 4))
+            .shadow(radius: 8)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
